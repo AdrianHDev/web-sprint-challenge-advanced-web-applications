465f3f38f83ddd301a8b4320f910360ed11aead6
AdrianHDev <adrianhensondev@gmail.com>
2021-06-18 11:50:56 -0400

    Set Up ColorList.test.js

--- src/components/ColorList.test.js
+++ src/components/ColorList.test.js
@@ -2,11 +2,44 @@ import React from 'react';
 import { render, screen} from "@testing-library/react";
 import ColorList from './ColorList';
 
+const colors = [
+    {
+        color: "aliceblue",
+        code: {
+          hex: "#f0f8ff",
+        },
+        id: 1,
+      },
+      {
+        color: "limegreen",
+        code: {
+          hex: "#99ddbc",
+        },
+        id: 2,
+      },
+      {
+        color: "aqua",
+        code: {
+          hex: "#00ffff",
+        },
+        id: 3,
+      },
+]
 test("Renders an empty list of colors without errors", () => {
+    render(<ColorList colors={[]}/>)
 });
 
 test("Renders a list of colors without errors", () => {
+    render(<ColorList colors={colors} />)
+
 });
 
-test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {
-});
+test("Renders the EditForm when editing = true and does not render EditForm when editing = false", async () => {
+    const {rerender} = render(<ColorList colors={colors} editing={true} />)
+    let editForm = screen.queryByTestId('editMenu')
+    expect(editForm).toBeDefined()
+    
+    rerender(<ColorList colors={colors} editing={false} />)
+    editForm = screen.queryByTestId('editMenu')
+    expect(editForm).toBeNull();
+}); 
